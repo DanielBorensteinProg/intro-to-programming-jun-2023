38992da648439cf0a72dfe9a9a6f320f2a94bb11
Daniel Borenstein <Daniel_Borenstein@progressive.com>
2023-06-22 11:42:16 -0400

    done with that version

--- BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
+++ BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
@@ -7,9 +7,17 @@ using System.Threading.Tasks;
 namespace Banking.UnitTests.BankAccount;
 public class GoldCustomersGetABonusOnDeposits
 {
-    [Fact(Skip = "Working on it")]
+    [Fact]
     public void BonusIsApplied()
     {
+        var account = new Account();
+        account.AccountType = LoyaltyLevel.Gold;
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+        var expectedNewBalance = openingBalance + amountToDeposit + 10M;
 
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(expectedNewBalance, account.GetBalance());
     }
 }
