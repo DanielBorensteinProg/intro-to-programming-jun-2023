fca4a00a4f88b5727c0c98f62101c6e4636aae79
Daniel Borenstein <Daniel_Borenstein@progressive.com>
2023-06-23 15:32:10 -0400

    need to setup my interface need to refactor :)

---
+++ GreeterPractice/Greeter.UnitTests/GreetingTests.cs
@@ -0,0 +1,113 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeter.UnitTests;
+public class GreetingTests
+{
+    private readonly GreetingMaker _greetingMaker;
+    private readonly Mock<IFindBadNames> _findBadNames;
+    
+    public GreetingTests()
+    {
+        _findBadNames = new Mock<IFindBadNames>();
+        _greetingMaker = new GreetingMaker(_findBadNames.Object);
+    }
+
+    [Theory]
+    [InlineData("Windom", "Hello, Windom!")]
+    [InlineData("Cooper", "Hello, Cooper!")]
+    public void SingleName(string name, string expected)
+    {
+        
+
+        string greeting = _greetingMaker.Greet(name);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData(null, "Hello, Daniel!")]
+    public void NullName(string name, string expected)
+    {
+
+        string greeting = _greetingMaker.Greet(name);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData("BOB", "HELLO, BOB!")]
+    [InlineData("JEFF", "HELLO, JEFF!")]
+    public void ExcitedName(string name, string expected)
+    {
+
+
+        string greeting = _greetingMaker.Greet(name);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData("Bob", "Sue", "Hello, Bob and Sue!")]
+    public void TwoNames(string name1,string name2, string expected)
+    {
+
+        string greeting = _greetingMaker.Greet(name1, name2);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData("Hello, Bob, Sue, and Jeff!", "Bob", "Sue","Jeff")]
+    [InlineData("Hello, Cole, Cooper, Rosenfield, Preston, Milford, and Jeffries!", "Cole", "Cooper", "Rosenfield", "Preston", "Milford", "Jeffries")]
+    public void MultipleNames(string expected, string name, params string[] moreNames)
+    {
+
+        string greeting = _greetingMaker.Greet(name, moreNames);
+
+        Assert.Equal(expected, greeting);
+    }
+
+
+    [Theory]
+    [InlineData("Hello, Bob, Jim, AND SUE!", "Bob", "SUE","Jim" )]
+    [InlineData("HELLO, COLE, COOPER, ROSENFIELD, PRESTON, MILFORD, AND JEFFRIES!", "COLE", "COOPER", "ROSENFIELD", "PRESTON", "MILFORD", "JEFFRIES")]
+    public void MixedShouting(string expected, string name, params string[] moreNames)
+    {
+
+        string greeting = _greetingMaker.Greet(name, moreNames);
+
+        Assert.Equal(expected, greeting);
+    }
+
+
+    [Theory]
+    [InlineData("Hello, Bob, Mike, and Karl!", "Bob", "Mike, Karl")]
+    [InlineData("HELLO, COLE, COOPER, ROSENFIELD, PRESTON, MILFORD, AND JEFFRIES!", "COLE", "COOPER", "ROSENFIELD", "PRESTON", "MILFORD", "JEFFRIES")]
+    public void SplitComma(string expected, string name, params string[] moreNames)
+    {
+
+
+        string greeting = _greetingMaker.Greet(name, moreNames);
+
+        Assert.Equal(expected, greeting);
+    }
+
+
+    [Theory]
+    [InlineData("Hello, Same, Carl, J****n, Bill, and B***y!", "Sam", "Carl", "Jayden", "Bill", "Buffy")]
+    public void BadNames(string expected, string name, params string[] moreNames)
+    {
+
+
+        string greeting = _greetingMaker.Greet(name, moreNames);
+
+        Assert.Equal(expected, greeting);
+    }
+
+
+}
---
+++ GreeterPractice/Greeter/GreetingMaker.cs
@@ -0,0 +1,201 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace Greeter
+{
+    public class GreetingMaker
+    {
+
+        private readonly IFindBadNames _findBadNames;
+
+        public GreetingMaker(IFindBadNames findBadNames)
+        {
+            _findBadNames = findBadNames;
+        }
+
+
+
+        public string Greet(string? name, params string[] moreNames)
+        {
+            if (name == null)
+            {
+                name = "Daniel";
+            }
+            bool upperHandler = false;
+            bool commaFound = false;
+
+
+            if (name != null && name.Contains(","))
+            {
+                commaFound = true;
+            }
+            foreach (string moreName in moreNames)
+            {
+                if (moreName != null && moreName.Contains(","))
+                {
+                    commaFound = true;
+                }
+            }
+            if (commaFound)
+            {
+                return commaHandler(name, moreNames);
+            }
+            List<string> lowerNames = new();
+            List<string> upperNames = new();
+
+            List<string> names = new();
+            names.Add(name);
+            names.AddRange(moreNames);
+
+            foreach (string n in names.ToArray())
+            {
+                if (n == n.ToUpper())
+                {
+                    upperNames.Add(n);
+                }
+                else
+                {
+                    lowerNames.Add(n);
+                }
+            }
+            lowerNames.AddRange(upperNames);
+            var namezarray = lowerNames.ToArray();
+            name = namezarray[0];
+            moreNames = namezarray.Skip(1).ToArray();
+            name = MultipleUsersNameHandler(name, moreNames);
+            //var cleanedNames = badNameFilter(name, moreNames);
+            //namezarray = cleanedNames.ToArray();
+            //name = namezarray[0];
+            //moreNames = namezarray.Skip(1).ToArray();
+            if (name == name.ToUpper()) { upperHandler = true; }
+            if (upperHandler)
+            {
+                return "HELLO, " + name + "!";
+            }
+            return "Hello, " + name + "!";
+        }
+
+        private List<string> badNameFilter(string? name, params string[] moreNames)
+        {
+            var badNames = _findBadNames.GetBadNames();
+            List<string> listOfNames = new List<string>();
+            List<string> cleanedNames = new List<string>();
+            listOfNames.Add(name);
+            listOfNames.AddRange(moreNames);
+            foreach (string n in listOfNames.ToArray())
+            {
+
+                foreach (string badName in badNames)
+                {
+                    string cleanedName;
+                    if (n.ToLower().Equals(badName.ToLower()))
+                    {
+                        cleanedName = n[0] + new string('*', name.Length - 2) + name[name.Length - 1];
+                    }
+                    else
+                    {
+                        cleanedName = n;
+                    }
+                    cleanedNames.Add(cleanedName);
+
+                }
+            }
+
+            return cleanedNames;
+        }
+
+        private string commaHandler(string? name, params string[] moreNames)
+        {
+
+            List<string> listToPutInArray = new();
+            string[] firstNames = name.Split(',');
+            foreach (string firstName in firstNames)
+            {
+                string trimmedFirstName = firstName.Trim();
+                if (trimmedFirstName != null && trimmedFirstName.Length > 0)
+                {
+                    listToPutInArray.Add(trimmedFirstName);
+                }
+            }
+            foreach (string moreName in moreNames)
+            {
+                string[] nextNames = moreName.Split(',');
+                foreach (string nextName in nextNames)
+                {
+                    string trimmedNextName = nextName.Trim();
+                    if (trimmedNextName != null && trimmedNextName.Length > 0)
+                    {
+                        listToPutInArray.Add(trimmedNextName);
+                    }
+                }
+            }
+
+            listToPutInArray.ToArray();
+            name = listToPutInArray[0];
+            moreNames = listToPutInArray.Skip(1).ToArray();
+            return Greet(name, moreNames);
+
+
+        }
+
+        private static string MultipleUsersNameHandler(string? name, params string[] moreNames)
+        {
+
+            var wordAnd = "and";
+            if (name != null && name == name.ToUpper())
+            {
+                wordAnd = "AND";
+            }
+            {
+
+            }
+            if (moreNames != null && moreNames.Length > 0)
+            {
+                for (int i = 0; i < moreNames.Length - 1; i++)
+                {
+                    string moreName = moreNames[i];
+                    if (moreName != null && moreName == moreName.ToUpper())
+                    {
+                        wordAnd = "AND";
+                    }
+                    if (moreName != null)
+                    {
+                        name += ", " + moreName;
+                    }
+
+                }
+                if (moreNames[moreNames.Length - 1] != null && moreNames[moreNames.Length - 1] == moreNames[moreNames.Length - 1].ToUpper())
+                {
+                    wordAnd = "AND";
+
+                }
+                if (moreNames.Length == 1)
+                {
+                    name += " " + wordAnd + " " + moreNames[moreNames.Length - 1]; //2 names
+                }
+                else
+                {
+                    name += ", " + wordAnd + " " + moreNames[moreNames.Length - 1]; //more than 2 names
+                }
+
+
+
+            }
+
+            return name;
+        }
+    }
+
+
+
+}
+
+
+public interface IFindBadNames
+{
+    List<string> GetBadNames()
+    {
+        List<string> listOfBadNames = new List<string>() { "Jayden", "Buffy" };
+        return listOfBadNames;
+    }
+}
